6a95a63a85d0090fc7f2e8d31c6d8c3ac2e0ba22
Yaroslav Hrabovets <yarikgrabovets14@gmail.com>
2023-04-30 23:42:42 +0100

    Change Base URL

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'https://yaroslavhrabovets.in.ua/country-api'
 })
 
 export default instance
